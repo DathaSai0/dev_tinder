cdcd21266784ae3f38715968f6083d4426e7cb3b
Dathasai0 <lakkojuleeladathasai@gmail.com>
2025-05-16 00:23:20 +0530

    cors issues

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const connectMangoDb = require("./config/database");
+const cors = require("cors");
 
 const cookieParser = require("cookie-parser");
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
